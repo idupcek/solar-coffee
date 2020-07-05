2c2c197818828352a92438a5252f703a17471b6f
idupcek <ivan.dupcek@gmail.com>
2020-07-05 16:05:38 +1200

    Charts

--- SolarCoffee.Web/Controllers/InventoryController.cs
+++ SolarCoffee.Web/Controllers/InventoryController.cs
@@ -3,6 +3,7 @@ using Microsoft.Extensions.Logging;
 using SolarCoffee.Services.Inventory;
 using SolarCoffee.Web.Serialization;
 using SolarCoffee.Web.ViewModels;
+using System;
 using System.Linq;
 
 namespace SolarCoffee.Web.Controllers
@@ -52,6 +53,55 @@ namespace SolarCoffee.Web.Controllers
             var inventory = _inventoryService.UpdateUnitsAvailable(id, adjustment);
 
             return Ok(inventory);
+        }
+
+        [HttpGet("/api/inventory/snapshot")]
+        public ActionResult GetSnapshotHistory()
+        {
+            /*returns data like this:
+             * {
+             *      timeline: [1, 2, 3, .. n],
+             *      inventory:[{ id: 1, qty: [43, 21, 32 .. n], {id: 2, qty: [43, 12, 43 .. n ]}]
+             * }
+             */
+
+            _logger.LogInformation("Getting snapshot history");
+
+            try
+            {
+                var snapshotHistory = _inventoryService.GetSnapshotHistory();
+
+                //Get distinct points in time a snapshot was collected
+                var timelineMarkers = snapshotHistory
+                    .Select(t => t.SnapshotTime)
+                    .Distinct()
+                    .ToList();
+
+                //Get quantities grouped by id.
+                var snapshots = snapshotHistory
+                    .GroupBy(hist => hist.Product, hist => hist.QuantityOnHand,
+                        (key, g) => new ProductInventorySnapshotModel
+                        {
+                            ProductId = key.Id,
+                            QuantityOnHand = g.ToList()
+                        })
+                        .OrderBy(hist => hist.ProductId)
+                        .ToList();
+
+                var viewModel = new SnapshotResponse
+                {
+                    Timeline = timelineMarkers,
+                    ProductInventorySnapshots = snapshots
+                };
+
+                return Ok(viewModel);
+            }
+            catch(Exception e)
+            {
+                _logger.LogError("Errorgetting snapshot history.");
+                _logger.LogError(e.StackTrace);
+                return BadRequest("Error retrieving history");
+            }
         }
 
     }
--- solarcoffee.frontend/src/services/inventory-service.ts
+++ solarcoffee.frontend/src/services/inventory-service.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import { IProductInventory } from '@/types/Product';
 import { IShipment } from '@/types/Shipment';
+import { IInventoryTimeline } from '@/types/InventoryGraph';
 
 /**
  * Inventory Service.
@@ -18,6 +19,11 @@ export class InventoryService {
       const result = await axios.patch(
         `${this.API_URL}/inventory`, shipment
       );
+      return result.data;
+    }
+
+    public async getSnapshotHistory(): Promise<IInventoryTimeline> {
+      const result = await axios.get(`${this.API_URL}/inventory/snapshot`);
       return result.data;
     }
 }
